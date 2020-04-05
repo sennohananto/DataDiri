fca947f316aca43a4ff0125c5afda71dce08a503
Senno Hananto <sennohananto@gmail.com>
2020-04-05 23:41:19 +0700

    Initial Commit

---
+++ DataDiri/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DataDiri
+//
+//  Created by Senno Hananto on 05/04/20.
+//  Copyright © 2020 Senno Hananto. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
